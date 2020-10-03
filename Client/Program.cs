2abbde660945cc41fc5c9993ddd22f9129eb5407
rafaelldi <rival.abdrakhmanov@outlook.com>
2020-10-03 10:25:45 +0300

    Add client

---
+++ Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.WebSockets;
+using Websocket.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var client = new WebsocketClient(new Uri("ws://localhost:5000/"));
+            client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+
+            client.Start();
+
+            client.Send("Hello world!");
+
+            Console.ReadKey();
+
+            client.Stop(WebSocketCloseStatus.NormalClosure, "Status");
+        }
+    }
+}
